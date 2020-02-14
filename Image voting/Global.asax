81f5db46d29b0e95eae2ece8d3702ed84acd1173
gcd17243 <50860864+gcd17243@users.noreply.github.com>
2020-02-14 18:16:17 +0700

    Add project files.

---
+++ Image voting/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Image_voting.MvcApplication" Language="C#" %>
---
+++ Image voting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Image_voting.Startup))]
+namespace Image_voting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
